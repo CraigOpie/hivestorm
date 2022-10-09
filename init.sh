3cbd443d0eca5b2fad6b436012737795e987c38d
Craig Opie <craig@deathstardev.com>
2022-10-09 00:56:25 +0000

    updated files

--- init.sh
+++ init.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
+grep '^sudo:.*$' /etc/group | cut -d: -f4 >> sudoUsers.txt
+systemctl list-units --all --type=service --no-pager | grep running >> current.services
+diff current.services default.services >> check.services
+sudo apt list --installed >> current.apps
+diff current.apps default.apps >> check.apps
+sudo echo "deb http://security.ubuntu.com/ubuntu/ xenial-security main universe" >> /etc/apt/sources.list
+sudo echo "deb http://us.archive.ubuntu.com/ubuntu/ xenial-updates main universe" >> /etc/apt/sources.list
+sudo echo "deb http://us.archive.ubuntu.com/ubuntu/ xenial-backports main universe" >> /etc/apt/sources.list
+sudo sed -i 's/APT::Periodic::Update-Package-Lists "0";/APT::Periodic::Update-Package-Lists "1";/g' /etc/apt/apt.conf.d/10periodic
+sudo apt update
+DEBIAN_FRONTEND=noninteractive apt-get install -y "vim"
 DEBIAN_FRONTEND=noninteractive apt-get install -y "aide"
 DEBIAN_FRONTEND=noninteractive apt-get install -y "vlock"
 DEBIAN_FRONTEND=noninteractive apt-get install -y "auditd"
+DEBIAN_FRONTEND=noninteractive apt-get install -y "audispd-plugins"
 DEBIAN_FRONTEND=noninteractive apt-get install -y "libpam-pwquality"
-sudo echo "\nallow-guest=false\n" >> /etc/lightdm/lightdm.conf
+sudo echo "allow-guest=false" >> /etc/lightdm/lightdm.conf
 sudo ./aideAConfig.sh
 sudo ./gnomeConfig.sh
 sudo ./sudoConfig.sh
